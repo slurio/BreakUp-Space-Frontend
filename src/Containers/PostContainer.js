eb83946af9f5b453e4d11042f7b05e63e8644fc7
kevinpark07 <kevin.park07@yahoo.com>
2020-12-22 16:00:49 -0500

    Redux included

---
+++ src/Containers/PostContainer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+const PostContainer = props => {
+    return()
+}
+
+const msp = state => {
+    return {
+        
+    }
+}
+
+const mdp = dispatch => {
+    return { }
+}
+
+export default connect(msp,mdp)(PostContainer);

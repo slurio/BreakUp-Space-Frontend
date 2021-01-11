1da4990c33e996681db2acd998614951290ab2d3
kevinpark07 <kevin.park07@yahoo.com>
2021-01-11 17:21:22 -0500

    modal for Post Gallery

--- src/Components/PostDisplay.js
+++ src/Components/PostDisplay.js
@@ -1,18 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PostCard from '../Components/PostCard';
 import {connect} from 'react-redux';
+import Modal from '@material-ui/core/Modal';
+
 
 const PostDisplay = props => {
-
+    const [open, setOpen] = useState(false);
 
     const renderPosts = () => {
          return props.posts.map(post => <PostCard key={post.id} info={post} />)
+    }
+
+    const handleOpen = () => {
+        setOpen(true);
+    }
+
+    const handleClose = () => {
+        setOpen(false);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(e.target)
     }
 
     return (
         <div>
             <h1>Post Display</h1>
             {renderPosts()}
+            <button onClick={handleOpen}>Write Post</button>
+            <Modal
+                open={open}
+                onClose={handleClose}
+            >
+                <div>
+                    <form onSubmit={handleSubmit}>
+                        <input type='textarea' placeholder='text goes here!'/>
+                        <button>Submit</button>
+                    </form>
+                </div>
+            </Modal>
         </div>
     )
 }

44d1aebc4c45cd85df5e319d29064ea7613c12ad
kevinpark07 <kevin.park07@yahoo.com>
2021-01-20 18:52:21 -0500

    Footer set-up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavBar from './Components/NavBar';
 import HomePage from './Components/HomePage';
+import Footer from './Components/Footer'
 import PostGallery from './Containers/PostGallery';
 import Quiz from './Components/Quiz';
 import Login from './Components/Login';
@@ -30,6 +31,7 @@ class App extends React.Component {
           <Route path={'/posts'} render={ () => <PostGallery/>} />
           <Route path={'/'} render={ () => <HomePage/>} />
         </Switch>
+        <Footer/>
       </div>
     );
   }
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
+import logo from '../asset/navbar_logo.png';
+import NavLink from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <h1>Footer</h1>
+            <Logo src={logo} alt='logo' />
+            <div>
+                <p>Interested in partnering with us?</p>
+                <p>Email</p>
+            </div>
+        </Container>
     )
 }
 
 export default Footer;
+
+const Logo = styled.img`
+  width: 20%;
+  margin-left: 20%;
+`
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-right: 25px;
+    width: 35vw;
+`

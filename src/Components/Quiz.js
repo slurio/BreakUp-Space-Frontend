1c7f8ab2bf1628c59a6903def4551313e9e5d3cf
slurio <samantha.lurio@gmail.com>
2021-02-17 11:48:29 -0500

    setup initial quiz format

--- src/Components/Quiz.js
+++ src/Components/Quiz.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Quiz = () => {
+    const [startQuiz, setStartQuiz] = useState(true);
+
+    const handleClick = () => {
+        setStartQuiz(false);
+    }
+
     return (
         <div>
-            <h1>Quiz!</h1>
+            {startQuiz ?
+            <>
+                <h3>Why is it time to say goodbye?</h3>
+                <ul onClick={handleClick}>
+                    <li>Not ready to date (theme: Not Ready For A Relationship Right Now)</li>
+                    <li>Bad timing (theme: Too Busy To Date)</li>
+                    <li>Friendzone (theme: Getting A Friendship vibe)</li>
+                    <li>No connection (theme: Not Feeling A Connection)</li>
+                </ul>
+            </>
+            :
+            <h3>Questions Start From Backend</h3>}
         </div>
     )
 }

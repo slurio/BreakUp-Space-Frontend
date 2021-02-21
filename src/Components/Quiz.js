11aad4cb183d4bc95ddc0bf3471d3fbe948933a0
slurio <samantha.lurio@gmail.com>
2021-02-21 18:45:18 -0500

    refactored quixiz code

---
+++ src/Components/Quiz.js
@@ -0,0 +1,101 @@
+import React, {useState} from 'react';
+import {connect} from 'react-redux';
+import QuestionCard from '../Components/QuestionCard';
+import ResultCard from '../Components/ResultCard';
+
+const Quiz = (props) => {
+    const [count, setCount] = useState(0);
+    const [messageSubject, setMessageSubject] = useState('');
+    const [messageTone, setMessageTone] = useState({'casual': 0, 'friendly': 0, 'direct': 0});
+    
+    const resetQuiz = () => {
+        props.resetQuiz();
+        setCount(0);
+        setMessageTone({'casual': 0, 'friendly': 0, 'direct': 0});
+    }
+
+    const nextQuestion = (answer) => {
+        if(count === 0 && props.topic !== 'friendzone') {
+            setMessageSubject(answer.innerText.toLowerCase());
+            setCount(count+1)
+            renderQuestions()
+        } else if (messageSubject === 'made me feel uncomfortable' && count === 1) {
+            setMessageTone(answer.innerText.toLowerCase())
+            setCount(props.questions.length)
+            renderQuestions()  
+        } else {
+            switch(parseInt(answer.id)) {
+                case 1:
+                    setMessageTone({...messageTone, casual: messageTone['casual']+1})
+                    break;
+                case 2:
+                    setMessageTone({...messageTone, friendly: messageTone['friendly']+1})
+                    break;
+                case 3:
+                    setMessageTone({...messageTone, direct: messageTone['direct']+1})
+                    break;
+                default:
+                    break;
+            }
+            setCount(count+1)
+            renderQuestions()
+        }
+    }
+
+    const renderMessage = () => {  
+        let selectedTone = '';
+        for (let tone in messageTone) {
+            if(selectedTone !== '' && messageTone[tone] > messageTone[selectedTone]) {
+                selectedTone = tone;
+            } else if (selectedTone === '') {
+                selectedTone = tone;
+            }
+        }
+
+        let message;
+
+        if (props.topic === 'no connection' && messageSubject === 'made me feel uncomfortable') {
+            message = props.messages.find(message =>  message.tone === messageTone && message.subject === messageSubject)
+        } else if(!messageSubject) {
+            message = props.messages.find(message => message.topic.theme === props.topic && message.tone === selectedTone)
+        } else {
+            message = props.messages.find(message => message.topic.theme === props.topic && message.tone === selectedTone && message.subject === messageSubject)
+        }
+
+        return <ResultCard message={message}/>
+    }
+
+    const renderQuestions = () => {
+        if (messageSubject === 'just not feeling it' && count === 1) {
+            setCount(count + 1)
+            let selectedQuestion = props.questions[count];
+            return  <QuestionCard handleClick={nextQuestion} key={selectedQuestion.id} question={selectedQuestion.question} answers={selectedQuestion.answers}/>
+        } else if(count < props.questions.length) {
+            if (props.questions) {
+                let selectedQuestion = props.questions[count]
+                return  <QuestionCard handleClick={nextQuestion} key={selectedQuestion.id} question={selectedQuestion.question} answers={selectedQuestion.answers}/>
+            }
+        } else {
+            return (
+                <>
+                    <h3>Done! Quiz Complete</h3>
+                    {renderMessage()}
+                    <button onClick={resetQuiz}>Try Again</button>
+                </>
+            )
+        }
+    }
+
+    return (
+        renderQuestions()
+    )
+}
+
+
+const msp = state => {
+    return {
+        messages: state.messages
+    }
+}
+
+export default connect(msp, null)(Quiz);
--- src/Containers/BreakupQuizContainer.js
+++ src/Containers/BreakupQuizContainer.js
@@ -2,19 +2,15 @@ import React, {useState} from 'react';
 import {connect} from 'react-redux';
 import QuestionCard from '../Components/QuestionCard';
 import ResultCard from '../Components/ResultCard';
+import Quiz from '../Components/Quiz';
 
 const BreakupQuizContainer = (props) => {
     const [startQuiz, setStartQuiz] = useState(true);
     const [topic, setTopic] = useState('')
     const [questions, setQuestions] = useState('');
-    const [count, setCount] = useState(0);
-    const [messageSubject, setMessageSubject] = useState('');
-    const [messageTone, setMessageTone] = useState({'casual': 0, 'friendly': 0, 'direct': 0});
 
     const resetQuiz = () => {
         setStartQuiz(true);
-        setCount(0);
-        setMessageTone({'casual': 0, 'friendly': 0, 'direct': 0});
     }
 
     const handleClick = (event) => {
@@ -23,83 +19,6 @@ const BreakupQuizContainer = (props) => {
         let topicQuestions = props.topics.find(topic => topic.theme === selectedTopic);
         setTopic(topicQuestions.theme);
         setQuestions(topicQuestions.quizzes);
-    }
-
-    const nextQuestion = (answer) => {
-        if(count === 0 && topic !== 'friendzone') {
-            setMessageSubject(answer.innerText.toLowerCase());
-            setCount(count+1)
-            renderQuestions()
-        } else if (messageSubject === 'made me feel uncomfortable' && count === 1) {
-            setMessageTone(answer.innerText.toLowerCase())
-            setCount(questions.length)
-            renderQuestions()  
-        // } else if (topic === 'no connection') {
-        //     setMessageTone(answer.innerText.toLowerCase())
-        //     setCount(count+1)
-        //     renderQuestions()
-        } else {
-            switch(parseInt(answer.id)) {
-                case 1:
-                    setMessageTone({...messageTone, casual: messageTone['casual']+1})
-                    break;
-                case 2:
-                    setMessageTone({...messageTone, friendly: messageTone['friendly']+1})
-                    break;
-                case 3:
-                    setMessageTone({...messageTone, direct: messageTone['direct']+1})
-                    break;
-                default:
-                    break;
-            }
-            setCount(count+1)
-            renderQuestions()
-        }
-    }
-
-    const renderMessage = () => {  
-        // which tone was most clicked / need case if all were clicked once
-        let selectedTone = '';
-        for (let tone in messageTone) {
-            if(selectedTone !== '' && messageTone[tone] > messageTone[selectedTone]) {
-                selectedTone = tone;
-            } else if (selectedTone === '') {
-                selectedTone = tone;
-            }
-        }
-
-        let message;
-
-        if (topic === 'no connection' && messageSubject === 'made me feel uncomfortable') {
-            message = props.messages.find(message =>  message.tone === messageTone && message.subject === messageSubject)
-        } else if(!messageSubject) {
-            message = props.messages.find(message => message.topic.theme === topic && message.tone === selectedTone)
-        } else {
-            message = props.messages.find(message => message.topic.theme === topic && message.tone === selectedTone && message.subject === messageSubject)
-        }
-
-        return <ResultCard message={message}/>
-    }
-
-    const renderQuestions = () => {
-        if (messageSubject === 'just not feeling it' && count === 1) {
-            setCount(count + 1)
-            let selectedQuestion = questions[count];
-            return  <QuestionCard handleClick={nextQuestion} key={selectedQuestion.id} question={selectedQuestion.question} answers={selectedQuestion.answers}/>
-        } else if(count < questions.length) {
-            if (questions) {
-                let selectedQuestion = questions[count]
-                return  <QuestionCard handleClick={nextQuestion} key={selectedQuestion.id} question={selectedQuestion.question} answers={selectedQuestion.answers}/>
-            }
-        } else {
-            return (
-                <>
-                    <h3>Done! Quiz Complete</h3>
-                    {renderMessage()}
-                    <button onClick={resetQuiz}>Try Again</button>
-                </>
-            )
-        }
     }
 
     return (
@@ -115,7 +34,7 @@ const BreakupQuizContainer = (props) => {
                 </ul>
             </>
             :
-            renderQuestions()}
+            <Quiz resetQuiz={resetQuiz} topic={topic} questions={questions}/>}
         </div>
     )
 }
@@ -123,7 +42,6 @@ const BreakupQuizContainer = (props) => {
 const msp = state => {
     return {
         topics: state.topics,
-        messages: state.messages
     }
 }
 

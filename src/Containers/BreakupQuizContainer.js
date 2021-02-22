a6993c56962245385974138a8764515cd6ec1d56
slurio <samantha.lurio@gmail.com>
2021-02-21 20:03:05 -0500

    quiz question css

--- src/Containers/BreakupQuizContainer.js
+++ src/Containers/BreakupQuizContainer.js
@@ -21,20 +21,22 @@ const BreakupQuizContainer = (props) => {
     }
 
     return (
-        <div>
-            {startQuiz ?
-            <>
-                <h3>Why is it time to say goodbye?</h3>
-                <TopicList onClick={handleClick}>
-                    <TopicLi>Not ready to date</TopicLi>
-                    <TopicLi>Bad timing</TopicLi>
-                    <TopicLi>Friendzone</TopicLi>
-                    <TopicLi>No connection</TopicLi>
-                </TopicList>
-            </>
-            :
-            <Quiz resetQuiz={resetQuiz} topic={topic} questions={questions}/>}
-        </div>
+        <QuizContainer>
+            <ScreenContainer>
+                {startQuiz ?
+                <div>
+                    <h3>Why is it time to say goodbye?</h3>
+                    <TopicList onClick={handleClick}>
+                        <TopicLi>Not ready to date</TopicLi>
+                        <TopicLi>Bad timing</TopicLi>
+                        <TopicLi>Friendzone</TopicLi>
+                        <TopicLi>No connection</TopicLi>
+                    </TopicList>
+                </div>
+                :
+                <Quiz resetQuiz={resetQuiz} topic={topic} questions={questions}/>}
+            </ScreenContainer>
+        </QuizContainer>
     )
 }
 
@@ -45,6 +47,22 @@ const msp = state => {
 }
 
 export default connect(msp, null)(BreakupQuizContainer);
+
+const QuizContainer = styled.div`
+    font-family: helvetica;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const ScreenContainer = styled.div`
+    width: 450px;
+    border: solid 1px grey;
+    display: flex;
+    flex-direction: column;
+    padding: 10px;
+    margin-top: 20px;
+`
 
 const TopicList = styled.ul`
     list-style-type: none;

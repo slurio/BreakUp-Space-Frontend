6fc6bf672a5472471e8a0717b574508d0c58cafd
kevinpark07 <kevin.park07@yahoo.com>
2021-03-03 17:22:13 -0500

    NavBar final updates

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,23 +3,22 @@ import {connect} from 'react-redux';
 import {loginUser} from '../Redux/actions';
 import {Link} from 'react-router-dom'
 import styled from 'styled-components';
-
+import Person from '@material-ui/icons/Person';
 
 
 function NavBar(props) {
   
-const logoutHandle = () => {
-    props.setUser('')
-}
 
   return (
     <Container>
-       <Tab to="/home"><Logo src='https://uploads-ssl.webflow.com/5e6437bd68556a7f88367dcc/5ec461df6e203d0a9b69f125_logo_new_1.png' alt='logo'/></Tab>
+      <Link to={props.user ? "/profile": "/login"}>
+        <Icon style={{fontSize: "24pt"}}/>
+      </Link>
+      <Tab to="/home"><Logo src='https://uploads-ssl.webflow.com/5e6437bd68556a7f88367dcc/5ec461df6e203d0a9b69f125_logo_new_1.png' alt='logo'/></Tab>
       <TabContainer>
+        <Tab to='/quiz' style={{color:"#78FF7D", fontWeight:"bold", textDecoration:"underline"}}>Quiz</Tab>
         <Tab to='/posts'>Community</Tab>
-        <Tab to='/quiz'>Quiz</Tab>
       </TabContainer>
-      
     </Container>
   )}
 
@@ -41,6 +40,14 @@ const Logo = styled.img`
   width: 200px;
   height: 30px;
 `
+const Icon = styled(Person)`
+  display: inline-block;
+  float: right;
+  color: white;
+  margin-right: 50px;
+  padding-top: 30px;
+  padding-bottom: 30px;
+`
 
 const Tab = styled(Link)`
   display:inline-block;
@@ -48,7 +55,7 @@ const Tab = styled(Link)`
   color: white;
   margin-left: 50px;
   text-decoration: none;
-  font-size: 12pt;
+  font-size: 14pt;
   text-transform: uppercase;
   padding-top: 30px;
   padding-bottom: 30px;
@@ -64,6 +71,7 @@ const TabContainer = styled.div`
   border-style: solid;
   border-color: transparent;
   height: 85px;
+  width: 50%;
 `
 
 
